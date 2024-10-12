9702cba1c27e2afa13b99db69718f0c0839bf7f3
janisfs <janisfs@mail.ru>
2024-10-13 00:26:09 +0300

    Добавил код в models.py (news) создал функцию __str__(self) в models.py (news) Создал БД (news) Сделал отладку отображения страницы новостей (news)

--- my_web_app/news/views.py
+++ my_web_app/news/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import News_post
 
 def news_home(request):
-    return render(request, 'news/news_home.html')  # Укажи путь к шаблону
+    news = News_post.objects.all()  # Получаем все новости из базы данных
+    return render(request, 'news/news_home.html', {'news': news})  # Укажи путь к шаблону
